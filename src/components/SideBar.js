ed8e2b92e5e957c33519ed6392c1df1460774c9a
hyungraelee <timgg12345@gmail.com>
2021-08-11 16:11:18 +0900

    fix Sidebar & add props in gamelistDB

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,34 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, Button } from 'antd';
+import { Card, Button, Divider, Row, Col } from 'antd';
 import {
 	FieldTimeOutlined,
 	TrophyOutlined,
 } from '@ant-design/icons';
 
-const SIDEBAR_WIDTH = 400;
-
-const SideBar = ({ id, title, place, description, playtime, level }) => {
+const SideBar = ({ id, info }) => {
 	return (
 		<div style={{
 			position: "sticky",
 			top: "20px",
 		}}>
-			<Card title="title" style={{ width: SIDEBAR_WIDTH, borderRadius: "10px", marginBottom: "5px" }}>
-				<p>place</p>
-				<p>description</p>
-				<div style={{
-					display: "flex",
-					gap: "30px",
-				}}>
-					<span style={{ display: "flex", gap: "5px" }}><FieldTimeOutlined />Playtime</span>
-					<span style={{ display: "flex", gap: "5px" }}><TrophyOutlined />Level</span>
-				</div>
+			<Card title={info.title} style={{ borderRadius: "10px", marginBottom: "5px" }}>
+				<p>장소: {info.place}</p>
+				<p>요약: {info.description}</p>
+				<Divider />
+				<Row>
+					<Col span={12}><FieldTimeOutlined /> Playtime: {info.playtime}</Col>
+					<Col span={12}><TrophyOutlined /> Level: {info.level}</Col>
+				</Row>
 				<Link to={`/games/${id}/play`}>
 					<Button type="primary" block style={{ marginTop: "20px" }}>Start!!!</Button>
 				</Link>
 			</Card>
-			<Card style={{ width: SIDEBAR_WIDTH, borderRadius: "10px" }}>
+			<Card style={{ borderRadius: "10px" }}>
 				<p>Goal Title</p>
 				<p>Goooooooooooooooal!!!</p>
 			</Card>
